4565941dabbdadcff1997b79e192adda9b9b842b
kaijesi <kaijesionowski@gmail.com>
2025-04-05 07:51:44 +0100

    Initial creation of Home Page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+{{-- Home Page Content --}}
+<x-base-layout>
+    <x-slot:title>ApolloWatts - Home</x-slot:title>
+    <h1>Welcome to ApolloWatts</h1>
+    <p>Your one-stop-shop for getting reliable analytics and forecasts for your home solar performance!</p>
+</x-base-layout>
