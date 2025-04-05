2d188a04dda3a2fb72f5c969facd76eec4b7c031
kaijesi <kaijesionowski@gmail.com>
2025-04-05 07:59:32 +0100

    Adjusted and added routing to currently available pages

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+/**
+ * Route to Home Page
+ */
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+/**
+ * Route to Signup Page
+ */
+Route::get('/signup', function () {
+    return view('signup');
+})->name('signup');
+
+/**
+ * Route to Register
+ */
+Route::post('/register', [UserController::class, 'store'])->name('register');
