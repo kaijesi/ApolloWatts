daf1f474ae3a01c01e5e0bbae70736a01d7ae3c8
kaijesi <kaijesionowski@gmail.com>
2025-04-14 17:30:26 +0100

    Improved the logic for the signup form JS controller, with irrelevant fields for each option (1. create new household OR 2. join existing) being set/unset to required & disabled. Also, there was previously an error in this where I essentially duplicated the logic of hiding and showing fields on both the intial load as well as the reaction to change of the radio button. By separating this hide/show logic into its own function "handleVisibility", this can now be invoked on the initial load of the page as well as when the householdOptions radio button is changed.

--- resources/js/helpers/signup-form.js
+++ resources/js/helpers/signup-form.js
@@ -3,31 +3,34 @@ function handleHouseholdJoinCheckbox() {
     const $householdOptions = $('input[name="householdOption"]');
     const $householdJoinFieldsContainer = $('#householdJoinFieldsContainer');
     const $householdCreateFieldsContainer = $('#householdCreateFieldsContainer');
+    const $householdInviteCode = $('#householdInviteCode');
 
     // Handle whether to show or hide the household creation fields
-    const selectedOption = $('input[name="householdOption"]:checked').val();
-    if (selectedOption === 'join') {
-        $householdJoinFieldsContainer.show();
-        $householdCreateFieldsContainer.hide();
-    } else {
-        $householdJoinFieldsContainer.hide();
-        $householdCreateFieldsContainer.show();
-    }
-
-    // React to changes of the radio button seelection
-    $householdOptions.change(function() {
+    function handleVisibility() {
         const selectedOption = $('input[name="householdOption"]:checked').val();
         if (selectedOption === 'join') {
             $householdJoinFieldsContainer.show();
+            $householdInviteCode.prop('required', true).prop('disabled', false); 
             $householdCreateFieldsContainer.hide();
+            $householdCreateFieldsContainer.find('input').prop('required', false).prop('disabled', true);
         } else {
             $householdJoinFieldsContainer.hide();
+            $householdInviteCode.prop('required', false).prop('disabled', true);
             $householdCreateFieldsContainer.show();
+            // Solis-realted fields are excluded from being set to required but should still be reenabled
+            $householdCreateFieldsContainer.find('input').not('#solis_api_id, #solis_api_key').prop('required', true);
+            $householdCreateFieldsContainer.find('input').prop('disabled', false);
         }
-    });
+    }
+
+    // Initial setup
+    handleVisibility();
+
+    // React to changes of the radio button selection
+    $householdOptions.change(handleVisibility);
 }
 
 // Execute when document is loaded
-$(document).ready(function() {
+$(document).ready(function () {
     handleHouseholdJoinCheckbox();
 })
