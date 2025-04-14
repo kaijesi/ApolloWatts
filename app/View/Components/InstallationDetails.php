4aef63b4605d8734c93a1d98d357f9619eb82ad1
kaijesi <kaijesionowski@gmail.com>
2025-04-14 17:51:30 +0100

    View logic for the Installation Details component which requires an installation object to be submitted to it for display

---
+++ app/View/Components/InstallationDetails.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class InstallationDetails extends Component
+{
+    
+    public $installation;
+    
+    /**
+     * Create a new component instance.
+     */
+    public function __construct($installation)
+    {
+        $this->installation = $installation;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.installation-details');
+    }
+}
