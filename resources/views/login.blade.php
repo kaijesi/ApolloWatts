e2de51428d6d1fc021d633543c4e24e000d16773
kaijesi <kaijesionowski@gmail.com>
2025-04-14 17:59:06 +0100

    Login page - This is for an initial design in which I've directed to this page via the navigation - In its current form, the "Login" navigation button will instead open a login modal showing the same login-form component used here - This page is retained to still allow for the /login route to be used just in case

---
+++ resources/views/login.blade.php
@@ -0,0 +1,9 @@
+{{-- Login Page Content --}}
+{{-- "Login" navigation button opens the login modal, 
+this view is how it was done before, retaining it to still allow for using /login route --}}
+
+<x-base-layout>
+    <x-slot:title>ApolloWatts - Login</x-slot:title>
+    <h1>Log into your Household Account</h1>
+    <x-login-form></x-login-form>
+</x-base-layout>
