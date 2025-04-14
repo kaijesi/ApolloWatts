5c561c3894fdb99be90c1405493c5f7ae7287ef8
kaijesi <kaijesionowski@gmail.com>
2025-04-14 17:54:54 +0100

    Modal blade component which allows for modification of its ID, Title, Content and Button action (note that for the button there is still some missing logic for it to actually do something. Also note that the button is not required, it will only be shown if a value is provided for modalAction).

---
+++ resources/views/components/modal.blade.php
@@ -0,0 +1,21 @@
+<div class="modal fade" id="{{ $modalId }}" tabindex="-1" aria-labelledby="{{ $modalTitle }}" aria-hidden="true">
+    <div class="modal-dialog modal-md">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="{{ $modalTitle }}">{{ $modalTitle }}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                {{ $slot }}
+            </div>
+            @if ($modalAction)
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">{{ $modalAction }}</button>
+                </div>
+            @else
+                
+            @endif
+        </div>
+    </div>
+</div>
