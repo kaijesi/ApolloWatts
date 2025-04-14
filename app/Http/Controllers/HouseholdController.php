6f2cb7673516b3f209333850657bca4ce02c308a
kaijesi <kaijesionowski@gmail.com>
2025-04-14 17:34:47 +0100

    Modified the household controller: - Added comments to indicate that the index, create and store standard functions that come with Laravel's controller creation don't apply, retaining them nonetheless in case they will in the future - Added logic for retrieving and showing the running user's household and its members

--- app/Http/Controllers/HouseholdController.php
+++ app/Http/Controllers/HouseholdController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\Household;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HouseholdController extends Controller
 {
     /**
      * Display a listing of the resource.
+     * --No use case at this point--
+     * As users can only belong to one household, there is currently no situation where 
+     * a list of household would need to be displayed
      */
     public function index()
     {
@@ -17,6 +21,8 @@ class HouseholdController extends Controller
 
     /**
      * Show the form for creating a new resource.
+     * --No use case at this point--
+     * A household will only ever be created as part of a user creation in the current scope of the app
      */
     public function create()
     {
@@ -25,6 +31,8 @@ class HouseholdController extends Controller
 
     /**
      * Store a newly created resource in storage.
+     * --No use case at this point--
+     * A household will only ever be stored as part of a user creation in the current scope of the app
      */
     public function store(Request $request)
     {
@@ -34,9 +42,13 @@ class HouseholdController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Household $household)
+    public function show()
     {
-        //
+        $user = Auth::user();
+        $household = $user->household;
+        $members = $household->users;
+        return view('my-household', ['household' => $household, 'members' => $members]);
+
     }
 
     /**
