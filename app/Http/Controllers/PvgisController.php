9b1b448ebe5479063f149a3a6a0308897ce750e7
kaijesi <kaijesionowski@gmail.com>
2025-04-16 22:52:38 +0100

    Added a controller for PVGIS related routes

---
+++ app/Http/Controllers/PvgisController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\PvgisRequest;
+use App\Services\PvgisClient;
+use Illuminate\Http\Request;
+
+class PvgisController extends Controller
+{
+    function showResults(PvgisRequest $request)
+    {
+        // Retireve validated data
+        $validatedData = $request->validated();
+        $client = new PvgisClient();
+        $results = $client->monthlyOutputProjection($validatedData);
+        //return view('analytics-results', ['results' => $results]);
+        return response($results);
+    }
+}
