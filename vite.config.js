835228811f61915bafbd87ac91db6f6db67deac7
kaijesi <kaijesionowski@gmail.com>
2025-04-16 22:50:11 +0100

    Added PVGIS analytics JavaScript file

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,10 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',                // Not used as Bootstrap covers styling
-                'resources/js/app.js',                  // Loaded on all pages
-                'resources/js/helpers/signup-form.js'   // Signup form related assets
+                'resources/css/app.css',                    // Not used as Bootstrap covers styling
+                'resources/js/app.js',                      // Loaded on all pages
+                'resources/js/helpers/signup-form.js',      // Signup form related assets
+                'resources/js/helpers/pvgis-request.js'     // PVGIS request methods
             ],
             refresh: true,
         }),
