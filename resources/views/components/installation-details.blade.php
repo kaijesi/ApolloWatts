4aca412efbe123329a07ea44dce50c09e1003e38
kaijesi <kaijesionowski@gmail.com>
2025-04-17 16:47:10 +0100

    Modified to show edit and delete options for household admin users

--- resources/views/components/installation-details.blade.php
+++ resources/views/components/installation-details.blade.php
@@ -7,6 +7,7 @@ Card component showing details for a specific installation
 <div class="card">
     <div class="card-header bg-secondary text-white d-flex justify-content-between align-items-center">
         <h5 class="mb-0">{{ $installation['name'] ?? 'Installation Details' }}</h5>
+        {{-- Only show "View Details" button for list view --}}
         @if (Route::currentRouteName() === 'my-installations')
             <div>
                 <a href="{{ route('installations.show', $installation['id']) }}" class="btn btn-sm btn-outline-light">
@@ -14,6 +15,35 @@ Card component showing details for a specific installation
                 </a>
             </div>
         @endif
+        {{-- Only show edit and delete buttons on individual views --}}
+        @if (Route::currentRouteName() === 'installations.show')
+            <div class="d-flex align-items-center" style="color: #212529;">
+                {{-- Only show edit button for authorised users --}}
+                @if (Auth::user()->can('update', $installation))
+                    <button type="button" class="btn btn-outline-light m-2" data-bs-toggle="modal"
+                        data-bs-target="#edit">
+                        Edit
+                    </button>
+                    <x-modal modalId="edit" modalTitle="Edit">
+                        <x-installation-form :isUpdate=true :installation='$installation'/>
+                    </x-modal>
+                @endif
+                {{-- Only show delete button for authroised users --}}
+                @if (Auth::user()->can('delete', $installation))
+                    <form action="{{ route('installations.destroy', $installation['id']) }}" method="POST"
+                        onsubmit="return confirm('Are you sure you want to delete this installation?')">
+                        @csrf
+                        {{-- Method must be specified as HTML forms only support post, 
+                        Laravel however can use this method parameter here an treat the post request as a DELETE --}}
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">
+                            Delete
+                        </button>
+                    </form>
+                @endif
+            </div>
+        @endif
+
     </div>
     <div class="card-body">
         <div class="mb-3">
