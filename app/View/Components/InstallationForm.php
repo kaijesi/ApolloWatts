e21dd3484e8eb69c4199c4893ed9f4e66602fec3
kaijesi <kaijesionowski@gmail.com>
2025-04-17 16:44:14 +0100

    Modified the logic here to allow the installation form to be used both for creation and editing of Installations

--- app/View/Components/InstallationForm.php
+++ app/View/Components/InstallationForm.php
@@ -5,16 +5,25 @@ namespace App\View\Components;
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
+use InvalidArgumentException;
 
 class InstallationForm extends Component
 {
-
+    public bool $isUpdate;
+    public $installation;
+    
+    // Allows to use the same form for create and update requests
     /**
      * Create a new component instance.
      */
-    public function __construct()
+    public function __construct($isUpdate=false, $installation='')
     {
-        //
+        $this->isUpdate = $isUpdate;
+        if ($isUpdate === true && $installation === null) {
+            throw new InvalidArgumentException("When isUpdate is true, an Installation object must be provided.");
+        }
+
+        $this->installation = $installation;
     }
 
     /**
