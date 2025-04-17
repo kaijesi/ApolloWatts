aa98f7e17b8b0aad5a2c2ace63f0e22645aae102
kaijesi <kaijesionowski@gmail.com>
2025-04-17 16:44:55 +0100

    User to submit the installation details to the solis-analytics Blade component

---
+++ app/View/Components/SolisAnalytics.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class SolisAnalytics extends Component
+{
+    public $installation;
+
+    /**
+     * Create a new component instance.
+     */
+    public function __construct($installation)
+    {
+        $this->installation = $installation;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.solis-analytics');
+    }
+}
