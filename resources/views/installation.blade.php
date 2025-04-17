e225b5c85564b691159e02eff1d74301cc8bfeda
kaijesi <kaijesionowski@gmail.com>
2025-04-17 16:46:31 +0100

    Added Solis Analytics section

--- resources/views/installation.blade.php
+++ resources/views/installation.blade.php
@@ -6,8 +6,9 @@ Contains details about an individual installation as well as analytics for it
 <x-base-layout>
     <x-slot:title>ApolloWatts - {{ $installation['name'] }} Details</x-slot:title>
     <h1>Installation Details</h1>
-    <h2>{{ $installation['name'] }}</h2>
+    <h2 class="my-4" id="installation-name" name="installation-name">{{ $installation['name'] }}</h2>
     <x-installation-details :installation='$installation' />
-    <h2>Analytics</h2>
-    <x-pvgis-analytics :installation='$installation' />
+    <h2 class="my-4">Analytics</h2>
+    <div class="my-4"><x-pvgis-analytics :installation='$installation' /></div>
+    <div class="my-4"><x-solis-analytics :installation='$installation'/></div>
 </x-base-layout>
