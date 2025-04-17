c201f55b5573a179497feb7f91b6df3a974c8a47
kaijesi <kaijesionowski@gmail.com>
2025-04-17 16:45:11 +0100

    Removed unused services, added solis

--- config/services.php
+++ config/services.php
@@ -14,29 +14,12 @@ return [
     |
     */
 
-    'postmark' => [
-        'token' => env('POSTMARK_TOKEN'),
-    ],
-
-    'ses' => [
-        'key' => env('AWS_ACCESS_KEY_ID'),
-        'secret' => env('AWS_SECRET_ACCESS_KEY'),
-        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-    ],
-
-    'resend' => [
-        'key' => env('RESEND_KEY'),
-    ],
-
-    'slack' => [
-        'notifications' => [
-            'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
-            'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
-        ],
-    ],
-
     'pvgis' => [
         'base_url' => env('PVGIS_BASE_URL')
-    ]
+    ],
+
+    'solis' => [
+        'base_url' => env('SOLIS_BASE_URL')
+    ],
 
 ];
