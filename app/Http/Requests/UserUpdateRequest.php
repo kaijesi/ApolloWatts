ff950b7efe4194502e9c08838b33e94229dea348
kaijesi <kaijesionowski@gmail.com>
2025-04-23 21:58:37 +0200

    Form request for updates to user information

---
+++ app/Http/Requests/UserUpdateRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class UserUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name'              => 'nullable|string|max:255',
+            'current-password'  => 'required_with:new-password',
+            'new-password'      => 'nullable',
+            'confirm-password'  => 'nullable|same:new-password',
+            'is-household-admin'=> 'nullable|bool'
+        ];
+    }
+}
